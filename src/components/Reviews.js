28135303503e6265be458229aa78e9239ff26c74
Mehul Jain <mehul.jain2206@gmail.com>
2026-03-31 12:34:36 +0530

    review6

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,13 +3,13 @@ import React from "react";
 
 const Reviews=(props)=>{
  let {id,name,job,image,text}=props.list;
-    return <div >
+    return < >
         {console.log("author-id",{id})}
-        <p className="author" id={`author-${id}`}>Author:{name}</p>
+        <p className="author">Author:{name}</p>
         <p className="job">Job:{job}</p>
         <img className="person-img" src={image} alt={name}></img>
         <p className="info"> Info:{text}</p>
-    </div>
+    </>
 
 }
 
